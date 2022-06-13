d8e9da6fd578872f0da234135c2c033e66e1d9a9
htmldav <htmldav@mail.ru>
2022-06-13 12:41:17 +0300

    nginx

---
+++ config.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Create a container
+resource "docker_container" "foo" {
+  image = docker_image.nginx.latest
+  name  = "foo"
+  ports {
+    internal = 80
+    external = 80
+  }
+}
